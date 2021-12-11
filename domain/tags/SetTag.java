f19cb852893583085a2667297f2cee3464ae3521
Dannyisco <95979287+Dannyisco@users.noreply.github.com>
2021-12-11 21:38:41 +0000

    Update SetTag.java

--- domain/tags/SetTag.java
+++ domain/tags/SetTag.java
@@ -4,6 +4,7 @@ import domain.shows.Show;
 /**
  * Interface that sets the tag information, restricted to the user.
  * 
+ */
 public interface SetTag extends Tag {
 	/**
 	 * Adds a show associated with this tag.
