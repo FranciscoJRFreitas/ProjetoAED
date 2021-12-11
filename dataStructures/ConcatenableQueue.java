faf8680ddc96c618d4b7b2c97d88efcec12c7141
CodingFreedomPT <80543161+CodingFreedomPT@users.noreply.github.com>
2021-12-11 23:00:55 +0000

    Update ConcatenableQueue.java

--- dataStructures/ConcatenableQueue.java
+++ dataStructures/ConcatenableQueue.java
@@ -2,8 +2,12 @@ package dataStructures;
 
 public interface ConcatenableQueue<E> extends Queue<E> {
 
-	// Removes all of the elements from the specified queue and
-	// inserts them at the end of the queue (in proper order).
+	/**
+	 * Removes all of the elements from the specified queue and inserts them at the
+	 * end of the queue (in proper order).
+	 * 
+	 * @param queue - The queue to append.
+	 */
 	void append(ConcatenableQueue<E> queue);
 
 }
