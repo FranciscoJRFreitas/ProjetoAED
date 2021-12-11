646deef37f9649ef8c0733c31e9dab161a75c68c
Dannyisco <95979287+Dannyisco@users.noreply.github.com>
2021-12-11 18:46:21 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -21,7 +21,7 @@ import domain.tags.Tag;
  * Main class for the application. Containing all the communication between the
  * user and the application.
  * 
- * AED 2021 - First Project - BdFI. Estou a alterar o projeto
+ * AED 2021 - First Project - BdFI.
  * 
  * @author Danny Fernandes 61162
  * @author Francisco Freitas 60313
